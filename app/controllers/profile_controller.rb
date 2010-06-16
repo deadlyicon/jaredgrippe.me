45745c3b4ecf4370f7b9515d5b87147c9e212b6b
Jared Grippe <jared@deadlyicon.com>
2010-06-16 15:49:42 -0700

    finished first pass at profile index

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,16 @@
 class ProfileController < ApplicationController
+
+  def index
+    @links = [
+      ["Google Profile", "http://www.google.com/profiles/deadlyicon"        ],
+      ["Twitter",        "http://twitter.com/deadlyicon"                    ],
+      ["Facebook",       "http://www.facebook.com/deadlyicon"               ],
+      ["Awesome Links",  "http://awesome.deadlyicon.com"                    ],
+      ["Flickr",         "http://www.flickr.com/deadlyicon"                 ],
+      ["Photos",         "http://www.flickr.com/people/deadlyicon/photosof/"],
+      ["Code",           "http://www.github.com/deadlyicon"                 ],
+      ["Resume",         "/resume"                                          ],
+    ]
+  end
+
 end
