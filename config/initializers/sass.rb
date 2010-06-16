1d9d9899d1c359ace06ed54ae5fcc6743fdd6957
Jared Grippe <jared@deadlyicon.com>
2010-06-16 16:35:53 -0700

    moving compiled stylesheets into temp dir

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,1 +1,5 @@
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
+Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'tmp/stylesheets' }
+
+require "fileutils"
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+Rails.application.config.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets"])
