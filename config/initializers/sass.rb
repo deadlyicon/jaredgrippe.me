2918bd76f9965ef3e2ea2b2ef3c5dcf91126d0a4
Jared Grippe <jared@deadlyicon.com>
2010-06-16 16:42:22 -0700

    Revert "moving compiled stylesheets into temp dir"
    This reverts commit 1d9d9899d1c359ace06ed54ae5fcc6743fdd6957.

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,5 +1,1 @@
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'tmp/stylesheets' }
-
-require "fileutils"
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
-Rails.application.config.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets"])
+Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
