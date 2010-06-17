6956bf0d95324ae0e95257f7817a5126b9f6b705
Jared Grippe <jared@deadlyicon.com>
2010-06-17 12:29:18 -0700

    added redirect to resume google doc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 JaredgrippeMe::Application.routes.draw do |map|
   match 'stylesheets/application.css' => 'stylesheets#application', :as => :application_stylesheet
   match 'javascripts/application.js'  => 'javascripts#application', :as => :application_javascript
+  match 'resume' => redirect("https://docs.google.com/View?id=dfk6v52k_55n5cjw9d7")
+
   root :to => "profile#index"
   match '*path' => "application#not_found"
 end
