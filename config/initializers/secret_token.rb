a142c4ecfc4280816b0f99c164b8254194fb8462
Jared Grippe <tech_ops+jared@change.org>
2012-11-04 12:12:59 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JaredgrippeMe::Application.config.secret_token = 'af07918f0889034720bc6dd35451f3888691538eba332bd8c128de10975b3af383095d4fb974db693b5707153446d547da07dcb10a38b7f85e4636a04abe4767'
